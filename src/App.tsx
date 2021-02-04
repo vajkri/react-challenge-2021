f692902668ed092a4a4c8489949e95e160c9bb55
Kriszta <krisztavajda@gmail.com>
2021-02-04 23:35:31 +0100

    Clean up App component and ongoing work on Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react'
 import { Header } from './organisms/Header/Header'
-import logo from './logo.svg'
-import './App.scss'
 
 function App() {
 	return (
-		<div className="App">
+		<>
 			<Header />
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<h1>Something funny</h1>
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
+			<div className="u-container u-mt-lg">page content coming soon...</div>
+		</>
 	)
 }
 
