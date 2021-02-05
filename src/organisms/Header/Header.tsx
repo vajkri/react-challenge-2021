48b562578f3e6ca98732fd5b8f64dbaf760d555f
Kriszta <krisztavajda@gmail.com>
2021-02-05 23:36:46 +0100

    Update Header + use Button there

--- src/organisms/Header/Header.tsx
+++ src/organisms/Header/Header.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import classNames from 'classnames'
 import styles from './Header.module.scss'
 import { Link } from '../../atoms/Link/Link'
+import { Button } from '../../molecules/Button/Button'
 
 export const Header: React.FunctionComponent = () => {
+	const handleTrialClick = () => {}
+
 	return (
 		<header className={styles.wrapper}>
 			<div className={classNames(styles.innerWrapper, 'u-container')}>
@@ -11,8 +14,10 @@ export const Header: React.FunctionComponent = () => {
 					DEMO Streaming
 				</Link>
 				<div className={styles.buttonWrapper}>
-					<button>Log in</button>
-					<button className="u-ml-xs">Start your free trial</button>
+					<Link href="#!">Log in</Link>
+					<Button className="u-ml-base" color={'dark'} onClick={handleTrialClick}>
+						Start your free trial
+					</Button>
 				</div>
 			</div>
 		</header>
