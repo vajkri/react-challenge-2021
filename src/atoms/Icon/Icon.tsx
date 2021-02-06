46fb8c36b7659b90e122ec67fa0fb25552f5a72b
Kriszta <krisztavajda@gmail.com>
2021-02-06 22:23:44 +0100

    Update content on Home page

--- src/atoms/Icon/Icon.tsx
+++ src/atoms/Icon/Icon.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
-import styles from './Icon.module.scss'
 import classNames from 'classnames'
+import styles from './Icon.module.scss'
 
 interface IconProps {
 	className?: string
 	id: string
+	size?: 'md' | 'lg'
 }
 
 export const Icon: React.FunctionComponent<IconProps> = props => {
 	return (
-		<i className={classNames(styles.wrapper, props.className)}>
+		<i
+			className={classNames(
+				styles.wrapper,
+				{ [styles.sizeMd]: props.size === 'md' || props.size === undefined, [styles.sizeLg]: props.size === 'lg' },
+				props.className
+			)}>
 			<svg>
 				<use href={'#' + props.id}></use>
 			</svg>
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Link } from '../../atoms/Link/Link'
+import styles from './HomePage.module.scss'
+import { Icon } from '../../atoms/Icon/Icon'
 
 export const HomePage: React.FunctionComponent = () => {
+	const renderCategoryLink = (category: string, title: string) => (
+		<article className={styles.catLink}>
+			<div className={styles.catLinkImgWrapper}>
+				<Icon id={'clapperboard'} size={'lg'} />
+				<h2 className={styles.catLinkCategory}>{category}</h2>
+			</div>
+			<p className={'u-mt-sm'}>{title}</p>
+		</article>
+	)
+
 	return (
-		<div>
-			<Link href={'/movies'}>Movies</Link>
-			<Link href={'/series'}>Series</Link>
+		<div className={styles.wrapper}>
+			<Link className={styles.catLinkWrapper} href={'/series'}>
+				{renderCategoryLink('Series', 'Popular Series')}
+			</Link>
+			<Link className={styles.catLinkWrapper} href={'/movies'}>
+				{renderCategoryLink('Movies', 'Popular Movies')}
+			</Link>
 		</div>
 	)
 }
