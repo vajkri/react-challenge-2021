c67ef2019e5d6a850929f9df771d70d2fe6ab663
Kriszta <krisztavajda@gmail.com>
2021-02-06 15:21:13 +0100

    Style Page title, get title depending on page

--- src/organisms/PageContent/PageContent.tsx
+++ src/organisms/PageContent/PageContent.tsx
@@ -3,24 +3,30 @@ import { Route, Switch } from 'react-router-dom'
 import { HomePage } from '../../pages/HomePage/HomePage'
 import { MoviesPage } from '../../pages/MoviesPage/MoviesPage'
 import { SeriesPage } from '../../pages/SeriesPage/SeriesPage'
+import styles from './PageContent.module.scss'
+import classNames from 'classnames'
 
 export const PageContent: React.FunctionComponent = () => {
+	const renderPageContent = (title: string, page: JSX.Element) => {
+		return (
+			<>
+				<div className={styles.titleWrapper}>
+					<h1 className={classNames(styles.title, 'u-container')}>{title}</h1>
+				</div>
+				<div className="u-container u-mt">{page}</div>
+			</>
+		)
+	}
+
 	return (
 		<main>
-			<h1>Page title</h1>
-			<div className="u-container u-mt">
-				<Switch>
-					<Route exact path="/">
-						<HomePage />
-					</Route>
-					<Route path="/movies">
-						<MoviesPage />
-					</Route>
-					<Route path="/series">
-						<SeriesPage />
-					</Route>
-				</Switch>
-			</div>
+			<Switch>
+				<Route exact path="/">
+					{renderPageContent('Popular Titles', <HomePage />)}
+				</Route>
+				<Route path="/movies">{renderPageContent('Popular Movies', <MoviesPage />)}</Route>
+				<Route path="/series">{renderPageContent('Popular Series', <SeriesPage />)}</Route>
+			</Switch>
 		</main>
 	)
 }
